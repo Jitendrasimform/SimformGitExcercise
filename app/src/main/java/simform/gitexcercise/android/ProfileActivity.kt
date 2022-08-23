4ddcdd7daa29a01e55109c3e895e35a063d35955
Jitendra <jitendra.simformsolutions@gmail.com>
2022-08-23 20:29:48 +0530

    - Add init on profile UI amend

--- app/src/main/java/simform/gitexcercise/android/ProfileActivity.kt
+++ app/src/main/java/simform/gitexcercise/android/ProfileActivity.kt
@@ -7,5 +7,7 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        init()
     }
+    private fun init(){}
 }
